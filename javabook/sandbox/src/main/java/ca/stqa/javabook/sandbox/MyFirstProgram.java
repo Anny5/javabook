d1361ea3be9283026147ff1509f38505333c6ba2
Anny5 <Ann.hamiltonsen@gmail.com>
2017-04-17 15:10:00 -0400

    Изменение настроек

--- javabook/sandbox/src/main/java/ca/stqa/javabook/sandbox/MyFirstProgram.java
+++ javabook/sandbox/src/main/java/ca/stqa/javabook/sandbox/MyFirstProgram.java
@@ -2,8 +2,8 @@ package ca.stqa.javabook.sandbox;
 
 public class MyFirstProgram {
 
-public static void main(String[] args){
+  public static void main(String[] args) {
 
-System.out.println("Hello world!");
-}
+    System.out.println("Hello world!");
+  }
 }
