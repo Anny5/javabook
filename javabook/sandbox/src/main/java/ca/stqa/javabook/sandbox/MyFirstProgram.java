40c68141036498e1853edc1db9cd1ae07df2ccd7
Anny5 <Ann.hamiltonsen@gmail.com>
2017-04-18 19:57:24 -0400

    myfirstprogram

--- javabook/sandbox/src/main/java/ca/stqa/javabook/sandbox/MyFirstProgram.java
+++ javabook/sandbox/src/main/java/ca/stqa/javabook/sandbox/MyFirstProgram.java
@@ -3,7 +3,27 @@ package ca.stqa.javabook.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
+    system("is working");
+    system("is sleeping");
+    system("is running");
 
-    System.out.println("Hello world!");
+    double l = 5;
+    System.out.println("Area of square of " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Area of rectangle with sides " + a + " and " + b + " = " + area(a, b));
+  }
+
+  public static void system(String somebody) {
+    System.out.println("The system " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
   }
 }
