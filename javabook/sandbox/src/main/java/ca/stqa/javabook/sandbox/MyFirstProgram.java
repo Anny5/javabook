f9f67abb6a6784ae4a8e4ebbbd0e99a422f6b182
Anny5 <Ann.hamiltonsen@gmail.com>
2017-04-26 22:18:58 -0400

    Added several functions

--- javabook/sandbox/src/main/java/ca/stqa/javabook/sandbox/MyFirstProgram.java
+++ javabook/sandbox/src/main/java/ca/stqa/javabook/sandbox/MyFirstProgram.java
@@ -3,27 +3,27 @@ package ca.stqa.javabook.sandbox;
 public class MyFirstProgram {
 
   public static void main(String[] args) {
-    system("is working");
-    system("is sleeping");
-    system("is running");
+    hello("world");
+    hello("user");
+    hello("Ang");
 
     double l = 5;
-    System.out.println("Area of square of " + l + " = " + area(l));
+    System.out.println("Area of square with side length of " + l + " = " + area(l));
 
     double a = 4;
     double b = 6;
-    System.out.println("Area of rectangle with sides " + a + " and " + b + " = " + area(a, b));
+    System.out.println("Area of rectangle with sides length of " + a + " and " + b + " = " + area(a, b));
   }
 
-  public static void system(String somebody) {
-    System.out.println("The system " + somebody + "!");
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
 
   public static double area(double len) {
     return len * len;
   }
 
-  public static double area(double a, double b){
+  public static double area(double a, double b) {
     return a * b;
   }
 }
